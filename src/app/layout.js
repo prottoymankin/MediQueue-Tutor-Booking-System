ef63bf49a8bf3aee89f4401af09f4067d44aa27a
Prottoy Mankin <prottoy032020@gmail.com>
2026-05-18 12:40:04 +0600

    Added footer section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
+import Footer from "@/components/shared/Footer";
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
       <body className="min-h-full flex flex-col">
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/shared/Footer.jsx
@@ -0,0 +1,76 @@
+import Link from "next/link";
+import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
+import { IoMdCall, IoMdMail } from "react-icons/io";
+import { IoLocationSharp } from "react-icons/io5";
+
+const Footer = () => {
+  return (
+    <footer className="bg-[url('/footer.svg')] bg-center bg-cover bg-no-repeat  py-15 text-white">
+      <div className="max-w-7xl mx-auto px-4 space-y-10">
+        <header>
+          <Link 
+            className="font-bold text-[28px]" 
+            href={"/"}
+          >
+            <span className="text-blue-500">M</span>
+            edi
+            <span className="text-blue-500">Q</span>
+            ueue
+          </Link>
+
+          <p className="max-w-xl">
+            MediQueue simplifies online tutor booking by helping students find expert tutors, schedule learning sessions, and manage classes seamlessly through a smart and organized platform.
+          </p>
+        </header>
+
+        <div className="gap-6 grid grid-cols-3">
+          <div className="space-y-4">
+            <h3 className="font-medium text-2xl">Services</h3>
+            <ul className="space-y-2">
+              <li>About us</li>
+              <li><Link href={"/tutors"}>Tutors</Link></li>
+              <li><Link href={"/add-tutors"}>Add Tutors</Link></li>
+              <li><Link href={"/booked-sessions"}>Booked Sessions</Link></li>
+              <li>Terms and conditions</li>
+            </ul>
+
+          </div>
+
+          <div className="space-y-4">
+            <h3 className="font-medium text-2xl">Contact</h3>
+            <ul className="space-y-2">
+              <li className="flex gap-2 items-center">
+                <IoMdMail />
+                support@mediqueue.com
+              </li>
+              <li className="flex gap-2 items-center">
+                <IoMdCall />
+                01799887766
+              </li>
+              <li className="flex gap-2 items-center max-w-sm">
+                <IoLocationSharp />
+                Suite 302, Education Hub Tower, Banani, Dhaka 1213, Bangladesh
+              </li>
+            </ul>
+          </div>
+
+          <div className="space-y-4">
+            <h3 className="font-medium text-2xl">Social</h3>
+            <ul className="flex gap-4 items-center text-2xl">
+              <li><FaFacebook /></li>
+              <li><FaInstagram /></li>
+              <li><FaYoutube /></li>
+              <li><FaLinkedin /></li>
+            </ul>
+          </div>
+        </div>
+
+        <p className="text-center">
+          © {new Date().getFullYear()} MediQueue. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
